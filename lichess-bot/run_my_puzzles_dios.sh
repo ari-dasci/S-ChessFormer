9eca778c37c5802f52821635a6f6a85a27855162
Álvaro Rodríguez Gallardo <80212790+AlvaroRodriguezGallardo@users.noreply.github.com>
2025-02-24 11:26:32 +0100

    solving puzzles within queues

---
+++ lichess-bot/run_my_puzzles_dios.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#SBATCH --job-name=ChessSolvingPuzzles                 # Nombre del proceso
+#SBATCH --partition=dios                    # Cola para ejecutar
+#SBATCH --gres=gpu:1                         # Número de GPUs a usar
+
+# Verifica que se haya pasado un argumento
+if [ "$#" -ne 1 ]; then
+    echo "Uso: $0 <engine_name>"
+    exit 1
+fi
+
+ENGINE_NAME="$1"
+
+cd "$(pwd)"
+
+# Inicializa Conda para bash
+source /opt/anaconda/etc/profile.d/conda.sh
+
+# Verifica si el entorno 'lichess_bot' ya existe
+if ! conda env list | grep -q "lichess_bot"; then
+    echo "Creando entorno Conda 'lichess_bot'..."
+    conda create --name lichess_bot python=3.10 --yes
+else
+    echo "El entorno Conda 'lichess_bot' ya existe."
+fi
+
+# Activa el entorno Conda
+conda activate lichess_bot
+
+# Instala pip si es necesario
+conda install pip -y
+
+# Instala las dependencias desde el archivo requirements.txt
+pip install -r requirements.txt
+
+# Para usar GPU
+pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+# Exporta la variable de entorno PYTHONPATH
+export PYTHONPATH="$PWD/engines:$PYTHONPATH"
+
+# Ejecuta el script principal con el nombre del motor pasado como argumento
+python3 my_puzzles.py --engine_name "SolvingChessPuzzles"
+
+# Envía un correo cuando termine
+MAIL -s "Proceso lichess-bot.py finalizado" CORREO@gmail.com <<< "El proceso ha finalizado"
